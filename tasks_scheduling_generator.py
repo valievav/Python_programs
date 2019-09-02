a75706af79aa90b0cfa525c587d61402d270d5e3
Venus <valievavm@gmail.com>
2019-09-02 21:38:03 +0200

    adding number of iterations, creating sheets with results

--- tasks_scheduling_generator.py
+++ tasks_scheduling_generator.py
@@ -1,7 +1,8 @@
 import openpyxl, os, datetime
+from openpyxl.utils.cell import get_column_letter
 
 
-def generate_schedule(file_name, file_data, occurence_in_days, future_iterations, cwd=None):
+def generate_schedule(file_name, file_data, iterations_gap, iterations_number, cwd=None):
 
     # switch to cwd if passes as argument
     if cwd:
@@ -23,43 +24,42 @@ def generate_schedule(file_name, file_data, occurence_in_days, future_iterations
         task = sheet.cell(row=i, column=tasks_col).value
         date = sheet.cell(row=i, column=date_col).value
         if task not in all_tasks.keys():  # record unique tasks
-            all_tasks.setdefault(task, date)
-        if date > all_tasks[task]:  # overwrite with most recent date
-            all_tasks[task]= date
+            all_tasks.setdefault(task, [date])
+        if date > all_tasks[task][0]:  # overwrite with most recent date
+            all_tasks[task] = [date]
 
-    # create new sheet and record all_tasks
-    unique_tasks_sheet = "Tasks_and_last_execution_date" #TODO
-    if unique_tasks_sheet not in wb.sheetnames:
-        wb.create_sheet(unique_tasks_sheet)
-
-    tasks_sheet = wb[unique_tasks_sheet]
-    n = 1
-    for value in all_tasks.keys():
-        tasks_sheet[f"A{n}"] = value
-        tasks_sheet[f"B{n}"] = all_tasks[value]
-        n += 1
-
-    wb.save(file_name)
-
-    # calculate next execution day
-    # TODO - include future_iterations
-    for task, date in all_tasks.items():
-        delta_days = datetime.timedelta(occurence_in_days)
-        next_date = date + delta_days
-        while next_date.weekday() in (0, 5, 6):  # do not schedule for Monday and weekend
+    # calculate the next execution day
+    for task in all_tasks.keys():
+        for i in range(iterations_number):
+            date = all_tasks[task][i]
+            next_date = date + iterations_gap
+            while next_date.weekday() in (0, 5, 6):  # do not schedule for Monday and weekend
                 next_date += datetime.timedelta(1)
-    # TODO record next_date
-        print(next_date.weekday(), task, next_date)
+            all_tasks[task].append(next_date)
 
+    # create new sheet for results
+    sheet_name = "Tasks_and_execution_dates"
+    if sheet_name not in wb.sheetnames:
+        wb.create_sheet(sheet_name)
+        print(f"Created new sheet '{sheet_name}' in '{file_name}'")
+    result_sheet = wb[sheet_name]
 
-
+    # record all tasks in new sheet
+    n = 1
+    for task in all_tasks.keys():
+        result_sheet[f"A{n}"] = task
+        for i in range(1, len(all_tasks[task])+1):
+            result_sheet[f"{get_column_letter(i+1)}{n}"] = all_tasks[task][i-1]
+        n += 1
+    print(f"Recorded results into '{sheet_name}' sheet")
+    wb.save(file_name)
 
 
 working_directory = "D:\\Practice Python\\Tasks schedule generator"
 file_name = "Tasks_schedule.xlsx"
 file_data = {"sheet_name": "Schedule", "tasks_col": 1, "date_col": 2}
-occurence_days = 90
-future_iterations = 3
+iterations_gap = datetime.timedelta(90)
+iterations_number = 2
 
-generate_schedule(file_name, file_data, occurence_days, future_iterations, working_directory)
+generate_schedule(file_name, file_data, iterations_gap, iterations_number, working_directory)
 
