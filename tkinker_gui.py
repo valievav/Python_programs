f5ec693b2214feb90dba493e4985291ddda541b5
Venus <valievavm@gmail.com>
2019-10-10 23:56:51 +0200

    window formatting updates, small refactoring

--- tkinker_gui.py
+++ tkinker_gui.py
@@ -2,29 +2,32 @@ from tkinter import Tk, Label, Button, Entry, StringVar, filedialog
 import sys
 
 
-window = Tk()
-window.title("Input parameters")
-window.minsize(500, 200)
-
-ENTRY_TYPE = "Entry"
-FILE_TYPE = "Filedialog"
-params = [["File Keyword", "Entry", "file"],
-          ["Search keywords", "Entry", "Castle, Final Fantasy"],
-          ["Sheet Name", "Entry", "sheet"],
-          ["Working directory", "Filedialog", "D:\\PYTHON Practice"],
-          ["Exclusion folder name", "Entry", "Top Secret"]]
-
-
 def click_folder_path():
     select_directory = filedialog.askdirectory()
-    button.configure(text=select_directory)  # TODO getting global button param??
+    # display selected path as button text (truncate if too long)
+    button["text"] = select_directory if len(select_directory) < button["width"] \
+        else select_directory[:button["width"]-3]+'...'  # TODO getting global button param??
 
 
 def click_submit():
     pass
 
 
-# build 2-column grid with params label-input
+ENTRY_TYPE = "Entry"
+FILE_TYPE = "Filedialog"
+params = [["File Keyword", ENTRY_TYPE, "file"],
+          ["Search keywords", ENTRY_TYPE, "Castle, Final Fantasy"],
+          ["Sheet Name", ENTRY_TYPE, "sheet"],
+          ["Working directory", FILE_TYPE, "D:\\PYTHON Practice"],
+          ["Exclusion folder name", ENTRY_TYPE, "Top Secret"],
+          ]
+
+# create window
+window = Tk()
+window.title("Input parameters")
+window.minsize(450, 100)
+
+# build 2-column grid with label-input
 for i in range(len(params)):
     param_name = params[i][0]
     param_input_type = params[i][1]
@@ -32,24 +35,23 @@ for i in range(len(params)):
 
     # insert labels in the 1st column
     label = Label(window, text=param_name)
-    label.grid(row=i, sticky='E')
+    label.grid(row=i, sticky='E', padx=5)
 
-    # insert input in the 2nd column
+    # insert inputs in the 2nd column
     if param_input_type == ENTRY_TYPE:
         default_value = StringVar(window, value=param_default_value)
         entry = Entry(window, width=50, textvariable=default_value)
-        entry.grid(row=i, column=1)
+        entry.grid(row=i, column=1, sticky='W', padx=10)
     elif param_input_type == FILE_TYPE:
-        button = Button(window, text="Select PATH", command=click_folder_path, width=42)   # TODO if path too long - truncate and display part
-        button.grid(row=i, column=1, sticky='W')
+        button = Button(window, text="Select PATH", command=click_folder_path, width=42, anchor="w")
+        button.grid(row=i, column=1, sticky='W', padx=10)
     else:
         print(f"Unexpected input type '{param_input_type}' for parameter '{param_name}'. "
               f"Accepted only {ENTRY_TYPE}, {FILE_TYPE}.")
         sys.exit()
 
 submit_button = Button(window, text="SUBMIT", width=10, command=click_submit)
-submit_button.grid(row=10, column=1)
-
+submit_button.grid(row=len(params), column=1, sticky='E', pady=20, padx=10)
 
 window.mainloop()
 
