576c8b29d47f5ba1e440bae63fac37fe40141cb8
Venus <valievavm@gmail.com>
2019-10-12 23:53:24 +0200

    added desc, improvements, refactoring

--- tkinker_gui.py
+++ tkinker_gui.py
@@ -4,76 +4,100 @@ import os
 import itertools
 
 
-def click_folder_path(param_name):
-    dir_name = filedialog.askdirectory(initialdir=param_default_value if os.path.isdir(param_default_value) else os.getcwd())
-    button_changed_output[param_name] = dir_name if param_name in button_changed_output.keys() else button_changed_output.setdefault(param_name, dir_name)
+def get_user_parameters_with_gui(params_input: dict):
+    """
+    Creates tkinter GUI window with parameters and default values. \n
+    After user entered new parameters and hit 'Submit' button, it collects all values and returns params_output dict.\n
+    :param params_input: dict
+    :return: params_output dict
+    """
+
+    def click_folder_path(param_name: str):
+        """
+        Opens directory window and sets selected path to button_output dict.
+        :param param_name: str
+        :return:
+        """
+        default_dir = params_input[param_name][1]
+        new_dir_name = filedialog.askdirectory(initialdir=default_dir if os.path.isdir(default_dir) else os.getcwd())
+
+        # change button output if already provided (user re-selects path) or set a new value
+        button_output[param_name] = new_dir_name if param_name in button_output.keys() else button_output.setdefault(param_name, new_dir_name)
+
+    def get_gui_info():
+        """
+        Fetches all parameters outputs from user (for labels & buttons)
+        :return:
+        """
+
+        for param_name, (param_input_type, param_default_value, elem_object) in params_input.items():
+            if param_input_type == param_types['ENTRY_TYPE']:
+                params_output.setdefault(param_name, elem_object.get().strip())
+            elif param_input_type == param_types['FILE_TYPE']:
+
+                # use new value if directory button was changed, else - use default
+                if param_name in button_output.keys():
+                    params_output.setdefault(param_name, button_output[param_name])
+                else:
+                    params_output.setdefault(param_name, param_default_value)
+
+        window.destroy()
+
+    # create tkinter window
+    window = Tk()
+    window.title("Input parameters")
+    window.minsize(450, 100)
+
+    param_types = {"ENTRY_TYPE": 0, "FILE_TYPE": 1}
+    params_output = {}
+    button_output = {}
+
+    # build 2-column grid with label-input layout
+    grid_row = itertools.count()
+
+    for param_name, (param_input_type, param_default_value) in params_input.items():
+        row = next(grid_row)
+
+        # insert labels in the 1st column
+        label = Label(window, text=param_name)
+        label.grid(row=row, sticky='E', padx=5)
+
+        # insert inputs in the 2nd column
+        value = StringVar()
+        value.set(param_default_value)
+
+        if param_input_type == param_types['ENTRY_TYPE']:
+            elem = Entry(window, width=50, textvariable=value)
+        elif param_input_type == param_types['FILE_TYPE']:
+            elem = Button(window, text="Select PATH", command=lambda param_name=param_name: click_folder_path(param_name), width=42, anchor="w")
+        else:
+            print(f"Unexpected input type '{param_input_type}' for parameter '{param_name}'. "
+                  f"Accepted only {param_types['ENTRY_TYPE']}, {param_types['FILE_TYPE']}.")
+            sys.exit()
+
+        elem.grid(row=row, column=1, sticky='W', padx=10)
+        params_input[param_name].append(elem)
+
+    # collect user output for params
+    submit_button = Button(window, text="SUBMIT", width=10, command=lambda: get_gui_info())
+    submit_button.grid(row=len(params_input), column=1, sticky='E', pady=20, padx=10)
+
+    window.mainloop()
+    return params_output
 
 
-def get_gui_info():
+def main():
+    params_input = {"File Keyword": [0, "file"],
+                    "Search keywords": [0, "Castle, Final Fantasy"],
+                    "Sheet Name": [0, "sheet"],
+                    "Working directory": [1, "D:\PYTHON Practice"],
+                    "Exclusion folder name": [0, "Top Secret"],
+                    }
 
-    for param_name, (param_input_type, param_default_value, elem_object) in params_input.items():
-
-        if param_input_type == ENTRY_TYPE:
-            params_output.setdefault(param_name, elem_object.get().strip())
-        elif param_input_type == FILE_TYPE:
-            if param_name in button_changed_output.keys():  # if file button was changed
-                params_output.setdefault(param_name, button_changed_output[param_name])
-            else:  #if it stayed default
-                params_output.setdefault(param_name, param_default_value)
-
-    print(f"Param OUTPUT - {params_output}, {button_changed_output}\n")
-    window.destroy()
+    params_output = get_user_parameters_with_gui(params_input)
+    print(f"Param OUTPUT - {params_output}")
 
 
-params_input = {"File Keyword": [0, "file"],
-                "Search keywords": [0, "Castle, Final Fantasy"],
-                "Sheet Name": [0, "sheet"],
-                "Working directory": [1, "D:\PYTHON Practice"],
-                "Exclusion folder name": [0, "Top Secret"],
-                }
-
-# create window
-window = Tk()
-window.title("Input parameters")
-window.minsize(450, 100)
-
-ENTRY_TYPE = 0
-FILE_TYPE = 1
-
-params_output = {}
-button_changed_output = {}
-
-# build 3-column grid with label-input-button
-grid_row = itertools.count()
-
-for param_name, (param_input_type, param_default_value) in params_input.items():
-    row = next(grid_row)
-
-    # insert labels in the 1st column
-    label = Label(window, text=param_name)
-    label.grid(row=row, sticky='E', padx=5)
-
-    value = StringVar()
-    value_default = value.set(param_default_value)
-
-    # insert inputs in the 2nd column
-    if param_input_type == ENTRY_TYPE:
-        elem = Entry(window, width=50, textvariable=value)
-
-    elif param_input_type == FILE_TYPE:
-        elem = Button(window, text="Select PATH", command=lambda param_name=param_name: click_folder_path(param_name), width=42, anchor="w")
-    else:
-        print(f"Unexpected input type '{param_input_type}' for parameter '{param_name}'. "
-              f"Accepted only {ENTRY_TYPE}, {FILE_TYPE}.")
-        sys.exit()
-
-    elem.grid(row=row, column=1, sticky='W', padx=10)
-    params_input[param_name].append(elem)
-
-
-# collect all parameters when pressed submit button
-submit_button = Button(window, text="SUBMIT", width=10, command=get_gui_info)
-submit_button.grid(row=len(params_input), column=1, sticky='E', pady=20, padx=10)
-
-window.mainloop()
+if __name__ == "__main__":
+    main()
 
