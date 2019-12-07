fdb72e85172b2adcd5ecbfc1248bcb49014e282a
Venus <valievavm@gmail.com>
2019-12-07 23:59:06 +0100

    added get_place_ids, improvements

--- Flight_prices_tracker/get_api_prices.py
+++ Flight_prices_tracker/get_api_prices.py
@@ -2,20 +2,39 @@ import requests
 import json
 import pprint
 from configparser import ConfigParser
+import datetime
 
 
-def live_prices_create_session(api_key, cabin_class, country, currency, locale, origin_place, destination_place, outbound_date):
+def get_place_id(base_url: str, headers: dict, currency: str, search_city: str, search_country: str) -> dict:
+    """
+     Returns list of place_ids (1 city can have different ids)
+    """
 
-    url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0"
+    url = f"{base_url}autosuggest/v1.0/{currency}/{currency}/{locale_lang}/"
+    querystring = {"query": {search_city}}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    result = json.loads(response.text)
 
-    payload = f"cabinClass={cabin_class}&country={country}&currency={currency}&locale={locale}" \
+    place_ids = {}
+    for location_data in result['Places']:
+        if location_data['CountryName'].lower() == search_country.lower():
+            place_ids.setdefault(location_data['PlaceId'], location_data['PlaceName'])
+
+    return place_ids
+
+
+def live_prices_create_session(base_url: str, headers: dict, cabin_class: str, country: str, currency: str,
+                               locale_lang: str, origin_place: str, destination_place: str, outbound_date: str)-> str:
+    """
+     Creates Live Pricing Service Session that should be created before requesting price data.\n
+     See detailed documentation -> https://skyscanner.github.io/slate/#flights-live-prices
+    """
+
+    url = f"{base_url}pricing/v1.0"
+    payload = f"cabinClass={cabin_class}&country={country}&currency={currency}&locale={locale_lang}" \
               f"&originPlace={origin_place}&destinationPlace={destination_place}&outboundDate={outbound_date}" \
               f"&adults={adults_number}"
-    headers = {
-        'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-        'x-rapidapi-key': api_key,
-        'content-type': "application/x-www-form-urlencoded"
-    }
+    headers.setdefault('content-type', "application/x-www-form-urlencoded")
 
     response = requests.request("POST", url, data=payload, headers=headers)
     response.raise_for_status()
@@ -28,25 +47,25 @@ def live_prices_create_session(api_key, cabin_class, country, currency, locale, 
         Exception(f"Attention! Issue with API LIVE request - {response.text}")
 
 
-def live_prices_pull_results(session_key):
+def live_prices_pull_results(base_url: str, headers: dict, session_key: str) -> dict:
+    """
+    Returns Live API results of the previously created session.
+    """
 
-    url = f"https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/" \
-                       f"apiservices/pricing/uk2/v1.0/{session_key}"
+    url = f"{base_url}pricing/uk2/v1.0/{session_key}?pageIndex=0&pageSize=20"
 
     querystring = {"pageIndex": "0", "pageSize": "10"}
-
-    headers = {
-        'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-        'x-rapidapi-key': api_key
-    }
-
     response = requests.request("GET", url, headers=headers, params=querystring)
 
-    results = json.loads(response.text)
-    return results
+    if not response.status_code == 200:
+        raise Exception(f"{response.status_code} - {response.reason} - {response.content['ValidationErrors'][0]}")
+
+    result = json.loads(response.text)
+    return result
 
 
-def live_prices_get_min_price(results):
+def live_prices_get_min_price(results: dict) -> int:
+
     min_price = None
     for n in range(len(results["Itineraries"])):
         flight_itinerary = results["Itineraries"][n]["PricingOptions"][0]
@@ -59,28 +78,49 @@ def live_prices_get_min_price(results):
 
 
 if __name__ == "__main__":
+
+    # general params
     parser = ConfigParser()
     parser.read('config.ini')
-
     api_key = parser.get("API", "rapidapi_key")
-    print(api_key)
-    cabin_class = "Economy"
+
+    base_url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/"
+    headers = {
+        'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+        'x-rapidapi-key': api_key
+    }
+
+    search_city_from = "Krakow"
+    search_country_from = "Poland"
+    search_city_to = "Tokyo"
+    search_country_to = "Japan"
+
     country = "PL"
     currency = "UAH"
-    locale = "en-US"
+    locale_lang = "en-US"
+
     origin_place = "KRK-sky"
-    destination_place = "NRT-sky"
-    # destination_place = "TYOA-sky"
+    destination_place = "TYOA-sky"
     outbound_date = "2019-12-21"
+    cabin_class = "Economy"
     adults_number = 1
 
-    price_threshold = 11000
+    price_threshold = 15000
 
-    session_key = live_prices_create_session(api_key, cabin_class, country, currency, locale, origin_place, destination_place, outbound_date)
-    results = live_prices_pull_results(session_key)
+
+    place_ids_from = get_place_id(base_url, headers, currency, search_city_from, search_country_from)
+    place_ids_to = get_place_id(base_url, headers, currency, search_city_to, search_country_to)
+    print(place_ids_from, place_ids_to)
+
+    # get Live API results and log them into file
+    session_key = live_prices_create_session(base_url, headers, cabin_class, country, currency, locale_lang,
+                                             origin_place, destination_place, outbound_date)
+    results = live_prices_pull_results(base_url, headers, session_key)
+
+    with open(f"Results_{datetime.datetime.now()}.txt".replace(":", "-"), "w") as file:
+        file.writelines(results)
 
     pprint.pprint(results, width=1)
-    print("--------")
 
     min_price = live_prices_get_min_price(results)
     print(f">>> SUCCESS! Found flight price {min_price} < threshold {price_threshold}.") if min_price <= price_threshold\
