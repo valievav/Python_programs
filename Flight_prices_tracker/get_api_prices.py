de404b8e010c0bd091c0f43f72859e7395f33e99
Venus <valievavm@gmail.com>
2019-12-08 23:36:47 +0100

    added reruns on invalid responses, timer function, refactoring

--- Flight_prices_tracker/get_api_prices.py
+++ Flight_prices_tracker/get_api_prices.py
@@ -1,8 +1,10 @@
-import requests
+import datetime
 import json
 import pprint
+import time
 from configparser import ConfigParser
-import datetime
+
+import requests
 
 
 def get_place_id(base_url: str, headers: dict, currency: str, search_city: str, search_country: str) -> dict:
@@ -30,43 +32,77 @@ def live_prices_create_session(base_url: str, headers: dict, cabin_class: str, c
      See detailed documentation -> https://skyscanner.github.io/slate/#flights-live-prices
     """
 
-    url = f"{base_url}pricing/v1.0"
-    payload = f"cabinClass={cabin_class}&country={country}&currency={currency}&locale={locale_lang}" \
-              f"&originPlace={origin_place}&destinationPlace={destination_place}&outboundDate={outbound_date}" \
-              f"&adults={adults_number}"
-    headers.setdefault('content-type', "application/x-www-form-urlencoded")
+    # rerun function until request is created successfully
+    while True:
+        url = f"{base_url}pricing/v1.0"
+        payload = f"cabinClass={cabin_class}&country={country}&currency={currency}&locale={locale_lang}" \
+                  f"&originPlace={origin_place}&destinationPlace={destination_place}&outboundDate={outbound_date}" \
+                  f"&adults={adults_number}"
+        headers.setdefault('content-type', "application/x-www-form-urlencoded")
 
-    response = requests.request("POST", url, data=payload, headers=headers)
-    response.raise_for_status()
+        response = requests.request("POST", url, data=payload, headers=headers)
 
-    # check response - success if empty
-    if len(response.text) == 2:
-        session_key = response.headers["Location"].split("/")[-1]
-        return session_key
-    else:
-        Exception(f"Attention! Issue with API LIVE request - {response.text}")
+        try:
+            response.raise_for_status()
+            session_key = response.headers["Location"].split("/")[-1]
+            break
+        except requests.exceptions.HTTPError as err:
+            print(f"Occurred error {err}. Going to rerun function.")
+            timer()
+
+    return session_key
 
 
-def live_prices_pull_results(base_url: str, headers: dict, session_key: str) -> dict:
+def live_prices_pull_results(base_url: str, headers: dict, session_key: str) -> list:
     """
     Returns Live API results of the previously created session.
     """
 
-    url = f"{base_url}pricing/uk2/v1.0/{session_key}?pageIndex=0&pageSize=20"
+    # rerun function until response pulled successfully
+    all_results = []
+    while True:
+        url = f"{base_url}pricing/uk2/v1.0/{session_key}?pageIndex=0&pageSize=20"
+        querystring = {"pageIndex": "0", "pageSize": "10"}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        result = json.loads(response.text)
 
-    querystring = {"pageIndex": "0", "pageSize": "10"}
-    response = requests.request("GET", url, headers=headers, params=querystring)
+        if response.status_code == 200:
+            if result["Status"] == "UpdatesPending":  # get next results pages
+                all_results.append(result)
+                print("Response have status 'UpdatesPending'. Requesting more results.")
+                continue
+            print(f'Response status - {result["Status"]}. Moving on to the response processing.')
+            break
+        else:
+            print(f"Occurred error {response.status_code} - {response.reason} - {response.content}. "
+                  f"Going to rerun function.")
+            timer()
 
-    if not response.status_code == 200:
-        raise Exception(f"{response.status_code} - {response.reason} - {response.content['ValidationErrors'][0]}")
-
-    result = json.loads(response.text)
-    return result
+    return all_results
 
 
-def live_prices_get_min_price(results: dict) -> int:
+def timer(wait_time: int = 90)-> bool:
+    """
+    Returns True when passed certain amount of seconds.
+    """
 
-    min_price = None
+    now = time.time()
+    timer_time = now + wait_time
+    while now <= timer_time:
+        time.sleep(1)
+        now += 1
+
+    return True
+
+
+def get_min_price(results: dict):
+    """
+    Returns number of prices in dictionary and minimum price.
+    """
+
+    min_price = 0
+    prices_count = 0
+
     for n in range(len(results["Itineraries"])):
         flight_itinerary = results["Itineraries"][n]["PricingOptions"][0]
         if not min_price:
@@ -74,12 +110,45 @@ def live_prices_get_min_price(results: dict) -> int:
         else:
             if min_price > flight_itinerary["Price"]:
                 min_price = flight_itinerary["Price"]
-    return min_price
+                prices_count = n+1
+
+    return prices_count, min_price
+
+
+def get_live_api_prices(base_url: str, headers: dict, cabin_class: str, country: str, currency: str,
+                        locale_lang: str, origin_place: str, destination_place: str, outbound_date: str,
+                        prices_count_threshold: int)-> None:
+    """
+    Gets Live API results and logs then into file.\n
+    Live API retrieval consists of 2 parts: creating session and getting results.
+    Reruns program if number of results is low (incomplete data).
+    """
+
+    # rerun function until full response retrieved successfully
+    while True:
+        session_key = live_prices_create_session(base_url, headers, cabin_class, country, currency, locale_lang,
+                                                 origin_place, destination_place, outbound_date)
+        all_results = live_prices_pull_results(base_url, headers, session_key)
+
+        for results in all_results:
+            prices_count, min_price = get_min_price(results)
+
+            if prices_count <= prices_count_threshold:
+                continue
+
+            # record results into log file
+            with open(f"Results_{datetime.datetime.now()}.txt".replace(":", "-"), "w") as file:
+                file.writelines(json.dumps(results))
+
+            # print results and min price
+            pprint.pprint(results, width=1)
+            print(f">>> SUCCESS! Found flight price {min_price} < threshold {price_threshold}.") if min_price <= \
+                    price_threshold else print(f">>> No suitable flight. "
+                    f"Min price {min_price} > threshold {price_threshold}.")
+            break
 
 
 if __name__ == "__main__":
-
-    # general params
     parser = ConfigParser()
     parser.read('config.ini')
     api_key = parser.get("API", "rapidapi_key")
@@ -106,23 +175,12 @@ if __name__ == "__main__":
     adults_number = 1
 
     price_threshold = 15000
-
+    prices_count_threshold = 50
 
     place_ids_from = get_place_id(base_url, headers, currency, search_city_from, search_country_from)
     place_ids_to = get_place_id(base_url, headers, currency, search_city_to, search_country_to)
-    print(place_ids_from, place_ids_to)
+    print(place_ids_from, place_ids_to)  # TODO - incorporate into program as step # 1
 
-    # get Live API results and log them into file
-    session_key = live_prices_create_session(base_url, headers, cabin_class, country, currency, locale_lang,
-                                             origin_place, destination_place, outbound_date)
-    results = live_prices_pull_results(base_url, headers, session_key)
-
-    with open(f"Results_{datetime.datetime.now()}.txt".replace(":", "-"), "w") as file:
-        file.writelines(results)
-
-    pprint.pprint(results, width=1)
-
-    min_price = live_prices_get_min_price(results)
-    print(f">>> SUCCESS! Found flight price {min_price} < threshold {price_threshold}.") if min_price <= price_threshold\
-        else print(f">>> No suitable flight. Min price {min_price} > threshold {price_threshold}.")
+    get_live_api_prices(base_url, headers, cabin_class, country, currency, locale_lang,
+                        origin_place, destination_place, outbound_date, prices_count_threshold)
 
