47fa54ead29a911b181b954c64bf6f608a58640c
Venus <valievavm@gmail.com>
2020-01-13 00:57:29 +0100

    add more params to prepare_dataframes, remove prepare_multi_indexes as redundant

--- Compare_excel_data_with_pandas/prepare_df_and_compare.py
+++ Compare_excel_data_with_pandas/prepare_df_and_compare.py
@@ -9,14 +9,6 @@ def relocate_column_inplace(df: pd.DataFrame, column: str, index: int)->None:
     df.insert(index, col.name, col)
 
 
-def prepare_multi_indexes(df: pd.DataFrame, index_name: str = 'Year', index_names: iter = ('Companies', 'Year'))->None:
-    """
-    Creates additional index 'Year' and gives names to indexes
-    """
-    df.reset_index(level=1).rename({'level_1': index_name}, axis=1)
-    df.index.names = index_names
-
-
 def remove_and_rename_columns(df: pd.DataFrame, remove_col_ident: str, rename_col_ident: str)->None:
     """
     Removes and renames columns that ends with a certain identifier
@@ -28,8 +20,10 @@ def remove_and_rename_columns(df: pd.DataFrame, remove_col_ident: str, rename_co
             df.rename({f'{column}': f'{column.replace(rename_col_ident, "")}'}, axis=1, inplace=True)
 
 
-def prepare_dataframes(file_1: str, file_2: str, avg_sheet_name: str, cnt_sheet_name: str,
-                       general_sheet_name: str)->tuple:
+def prepare_dataframes(file_1: str, file_2: str,
+                       avg_sheet_name: str, cnt_sheet_name: str, general_sheet_name: str,
+                       index_names: iter, overall_col_name: str,
+                       averages_suffix: str, count_suffix: str)->tuple:
     """
     Uploads and reshapes dataframes into the common structure for the analysis.
     Works only for dataframes with specific structure like so:
@@ -55,27 +49,26 @@ def prepare_dataframes(file_1: str, file_2: str, avg_sheet_name: str, cnt_sheet_
     # get averages df
     df1_avg = pd.read_excel(io=file_1, sheet_name=avg_sheet_name, header=[1, 2], index_col=0)
     df1_avg = df1_avg.stack(level=0)  # arrange each year on top of each other
-    prepare_multi_indexes(df1_avg)  # prepare 2 indexes ('Company' and 'Year')
-    relocate_column_inplace(df1_avg, 'Overall', 0)  # move 'Overall' column to the front
+    df1_avg.index.names = index_names  # adding names to indexes
+    relocate_column_inplace(df1_avg, overall_col_name, 0)  # move overall column to the front
 
     # get count df
     df1_cnt = pd.read_excel(io=file_1, sheet_name=cnt_sheet_name, header=[1, 2], index_col=0)
     df1_cnt = df1_cnt.stack(level=0)
-    prepare_multi_indexes(df1_cnt)
-    print(df1_cnt)
-    relocate_column_inplace(df1_cnt, 'Overall', 0)
+    df1_cnt.index.names = index_names
+    relocate_column_inplace(df1_cnt, overall_col_name, 0)
 
     # get general averages+count df
     df2_avg_cnt = pd.read_excel(io=file_2, sheet_name=general_sheet_name, header=1, index_col=[0, 1])
 
     # create 2 separate df from the general one
-    df2_avg = df2_avg_cnt.copy()
-    remove_and_rename_columns(df2_avg, '_cnt', '_avg')
-    prepare_multi_indexes(df2_avg)
+    df2_avg = df2_avg_cnt.copy(deep=True)
+    remove_and_rename_columns(df2_avg, count_suffix, averages_suffix)
+    df2_avg.index.names = index_names
 
-    df2_cnt = df2_avg_cnt.copy()
-    remove_and_rename_columns(df2_cnt, '_avg', '_cnt')
-    prepare_multi_indexes(df2_cnt)
+    df2_cnt = df2_avg_cnt.copy(deep=True)
+    remove_and_rename_columns(df2_cnt, averages_suffix, count_suffix)
+    df2_cnt.index.names = index_names
     
     return df1_avg, df1_cnt, df2_avg, df2_cnt
 
--- Compare_excel_data_with_pandas/runner.py
+++ Compare_excel_data_with_pandas/runner.py
@@ -7,12 +7,23 @@ pd.set_option('display.max_rows', None)
 
 file_path_1 = "Data_Separate_Tables.xlsx"
 file_path_2 = "Data_General_Table.xlsx"
+avg_sheet_name='Analysis_averages'
+cnt_sheet_name='Analysis_count'
+general_sheet_name='Analysis'
+index_names = ['Companies', 'Year']
+overall_col_name = "Overall"
+averages_suffix = '_avg'
+count_suffix = '_cnt'
 
 df1_avg, df1_cnt, df2_avg, df2_cnt = prepare_dataframes(file_1=file_path_1,
                                                         file_2=file_path_2,
-                                                        avg_sheet_name='Analysis_averages',
-                                                        cnt_sheet_name='Analysis_count',
-                                                        general_sheet_name='Analysis')
+                                                        avg_sheet_name=avg_sheet_name,
+                                                        cnt_sheet_name=cnt_sheet_name,
+                                                        general_sheet_name=general_sheet_name,
+                                                        index_names=index_names,
+                                                        overall_col_name=overall_col_name,
+                                                        averages_suffix=averages_suffix,
+                                                        count_suffix=count_suffix)
 
 print(df1_avg)
 print(df1_cnt)
