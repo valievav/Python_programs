a97ab7f7aaf3b7ea32f3d025a15c8e4b8c074d60
Venus <valievavm@gmail.com>
2020-03-20 22:26:11 +0100

    Quick fix - pickled file renaming

--- Flight_prices_tracker/config.py
+++ Flight_prices_tracker/config.py
@@ -31,6 +31,6 @@ json_files_folder = "json_files"
 log_files_folder = "log_files"
 json_file = f"{datetime.datetime.now().strftime('%Y-%m-%d')}_for_{city_from}-{city_to}_xxx.json"
 log_file = f"Logs_{datetime.datetime.now()}.log".replace(":", "-")
-pickle_file = 'pickled_data.txt'
+pickle_file = 'pickled_date.txt'
 save_to_file = True
 log_files_to_keep = 10
