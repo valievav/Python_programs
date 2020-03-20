ccebf7f9e5b5b55d009229ace2dc2dc8a0092b77
Venus <valievavm@gmail.com>
2020-03-20 22:08:43 +0100

    Move parameters into config file, add '.gitignore'

---
+++ Flight_prices_tracker/config.py
@@ -0,0 +1,36 @@
+import datetime
+from Flight_prices_tracker.config_private_keys import *
+
+# RAPID_API
+base_url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/"
+headers = {'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+           'x-rapidapi-key': rapidapi_key}  # use private api key
+
+# MONGODB
+instance = 'mongodb://localhost:27017/'
+db = 'skyskanner'
+db_collection = 'itineraries'
+
+# REQUEST_PARAMS
+city_from = "Krakow"
+country_from = "Poland"
+city_to = "Tokyo"
+country_to = "Japan"
+country = "PL"
+currency = "UAH"
+locale_lang = "en-US"
+cabin_class = "Economy"
+adults_count = 1
+outbound_date = "2020-05-01"
+
+# ADDITIONAL_PARAMS
+days_to_request = 3
+price_threshold = 15000
+max_retries = 3
+json_files_folder = "json_files"
+log_files_folder = "log_files"
+json_file = f"{datetime.datetime.now().strftime('%Y-%m-%d')}_for_{city_from}-{city_to}_xxx.json"
+log_file = f"Logs_{datetime.datetime.now()}.log".replace(":", "-")
+pickle_file = 'pickled_data.txt'
+save_to_file = True
+log_files_to_keep = 10
--- Flight_prices_tracker/runner.py
+++ Flight_prices_tracker/runner.py
@@ -2,63 +2,24 @@
 Gets Live API results, records them into MongoDB, records into file and finds min price.
 """
 
-import datetime
 import os
-from configparser import ConfigParser
 from Flight_prices_tracker.custom_logger import create_logger
 from Flight_prices_tracker.files_cleaner import files_cleaner
 from Flight_prices_tracker.get_live_api_results import get_api_data_for_n_days
-from Flight_prices_tracker.mongodb import connect_to_mongodb, find_flights_with_low_prices
+from Flight_prices_tracker.mongodb_methods import connect_to_mongodb, find_flights_with_low_prices
+from Flight_prices_tracker.config import *
 
 
 def main():
 
-    # rapidapi
-    parser = ConfigParser()
-    parser.read('config.ini')
-    api_key = parser.get("API", "rapidapi_key")
-    base_url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/"
-    headers = {
-        'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-        'x-rapidapi-key': api_key
-    }
-
-    # mongodb
-    instance = 'mongodb://localhost:27017/'
-    db = 'skyskanner'
-    collection = 'itineraries'
-
-    # request params
-    city_from = "Krakow"
-    country_from = "Poland"
-    city_to = "Tokyo"
-    country_to = "Japan"
-    country = "PL"
-    currency = "UAH"
-    locale_lang = "en-US"
-    cabin_class = "Economy"
-    adults_count = 1
-    outbound_date = "2020-05-01"
-
-    # additional params
-    days_to_request = 3
-    price_threshold = 15000
-    max_retries = 3
-    json_files_folder = "json_files"
-    log_files_folder = "log_files"
-    json_file = f"xxx_{city_from}-{city_to}_from_{datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')}.json"
-    log_file = f"Logs_{datetime.datetime.now()}.log".replace(":", "-")
-    pickle_file = 'Pickled_data.txt'
-
     # create logger
     cwd = os.getcwd()
-    log_file_abs_path = os.path.join(cwd, log_files_folder, log_file)
-    logger = create_logger(log_file_abs_path)
+    logger = create_logger(os.path.join(cwd, log_files_folder, log_file))
 
     # connect to db
     collection = connect_to_mongodb(mongodb_instance=instance,
                                     mongodb=db,
-                                    mongodb_collection=collection,
+                                    mongodb_collection=db_collection,
                                     logger=logger)
 
     # get LIVE API results, record values to db
@@ -81,7 +42,7 @@ def main():
                             json_file=json_file,
                             collection=collection,
                             logger=logger,
-                            save_to_file=True)
+                            save_to_file=save_to_file)
 
     # find flights with price < threshold
     find_flights_with_low_prices(threshold=price_threshold,
@@ -93,7 +54,7 @@ def main():
     log_path_to_clean = os.path.join(cwd, log_files_folder)
     files_cleaner(path_to_clean=log_path_to_clean,
                   extension='log',
-                  to_keep_number=10,
+                  to_keep_number=log_files_to_keep,
                   logger=logger)
 
 
