8a2060f5626e670bc672ee815ac437d71c052b3f
Venus <valievavm@gmail.com>
2020-09-19 00:34:33 +0200

    Add short circuit evaluation to boolean_chain

--- Practice_exercises/training/boolean_chain.py
+++ Practice_exercises/training/boolean_chain.py
@@ -44,8 +44,53 @@ def boolean_xor(l: list)-> bool:
     return result
 
 
-l = [True]
+l = []
 print(boolean_and(l))
+
+# TESTS
+assert boolean_and([False, False]) == False
+assert boolean_and([False, True]) == False
+assert boolean_and([True, False]) == False
+assert boolean_and([True, True]) == True
+assert boolean_and([True, True, True, True]) == True
+assert boolean_and([True, True, True, False]) == False
+
+assert boolean_or([False, False]) == False
+assert boolean_or([False, True]) == True
+assert boolean_or([True, False]) == True
+assert boolean_or([True, True]) == True
+assert boolean_or([False, False, False, True]) == True
+assert boolean_or([False, False, False, False]) == False
+
+assert boolean_xor([False, False]) == False
+assert boolean_xor([False, True]) == True
+assert boolean_xor([True, False]) == True
+assert boolean_xor([True, True]) == False
+
+# SOLUTION with short circuit evaluation
+
+
+def boolean_and(l: list) -> bool:
+    if not l:
+        return False
+
+    # short circuit evaluation
+    return not(False in l)
+
+
+def boolean_or(l: list)-> bool:
+    if not l:
+        return False
+
+    # short circuit evaluation
+    return True in l
+
+
+def boolean_xor(l: list)-> bool:
+    if not l:
+        return False
+
+    return True in l and not all(l)
 
 # TESTS
 assert boolean_and([False, False]) == False
