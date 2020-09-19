1547a253659d44aa1c992499e6b3b40d99085488
Venus <valievavm@gmail.com>
2020-09-19 15:27:35 +0200

    Add mini tasks

--- Practice_exercises/training/mini_tasks.py
+++ Practice_exercises/training/mini_tasks.py
@@ -1,8 +1,15 @@
+# Related read
+# https://realpython.com/python-coding-interview-tips/#select-the-right-built-in-function-for-the-job
+# https://docs.python.org/3/library/functions.html#built-in-functions
+
+section_sep = ''
+
 # EVEN numbers
 x = [11,1,3,4,5,6,8,9,10,1,3]
 even_only = [i for i in x if i %2==0]
 print(even_only)
 
+print(section_sep)
 ##################################
 
 # get COUNT of particular element in list
@@ -23,6 +30,7 @@ print(Counter(word))
 sentence = "It was a really great trip"
 print(len(sentence.split()))
 
+print(section_sep)
 ###################################
 
 # FIND DUPES in array
@@ -40,6 +48,7 @@ print(deduped)
 # REMOVE DUPES option for ordered list
 print(list(dict.fromkeys(x)))
 
+print(section_sep)
 #####################################
 
 # SORT list
@@ -48,6 +57,7 @@ char.sort(key=str.lower)  # in-place
 print(char)
 print(sorted(char, key=str.lower))  # copy and sort
 
+print(section_sep)
 #####################################
 
 # REVERSE list order with slicing
@@ -56,3 +66,51 @@ print(l[::-1])
 
 # REVERSE list order with reverse iter
 print(list(reversed(l)))
+
+# REVERSE list order with reverse method
+l.reverse()
+print(l, )
+
+print(section_sep)
+######################################
+
+# get LENGTH for each element in a list
+words = ['asd', 'zxcvb', 'a', 'qwerty']
+print(list(map(len, words)))
+
+print(section_sep)
+#######################################
+
+# UPDATE each element of a list to upper
+print(list(map(lambda x: x.upper(), words)))
+
+# ADD 2 lists
+x1 = [1,2,3,4,5]
+x2 = [6,7,8,9,10]
+print(list(map(lambda x,y: x+y, x1, x2)))
+
+print(section_sep)
+#######################################
+
+# get EVERY 2nd element in a list
+x = [1,2,3,4,5,6]
+print(x[::2])
+print(section_sep)
+
+print(section_sep)
+####################################
+
+# Generate every possible COMBINATION between elements of a list (order doesn't matter)
+import itertools
+
+people = ['Mary', 'Jake', 'Sandy', 'Becca', 'Robin']
+print(list(itertools.combinations(people, r=4)))
+
+print(section_sep)
+##################################
+
+# Generate every possible PERMUTATIONS between elements of a list (order does matter)
+print(list(itertools.permutations(people, r=2)))
+
+print(section_sep)
+##################################
